a4ef4458fdbc932d912957ae7d37e78d0ce6f9a7
JethroHotep <jethrohotep@outlook.com>
2019-02-20 10:38:49 -0500

    force clean build each time on debug server startup

--- server/startup.sh
+++ server/startup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+set -e
 pushd ../
-mvn install
+mvn clean install
 popd
 cp -v ../target/*.jar plugins/
 java -Xmx4192M -Xms4192M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar spigot-1.13.2.jar -nojline
